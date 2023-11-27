8b01693b2d3cbd922886016c81ae1bcd5ab14543
satra2806 <satra2806@outlook.com>
2023-11-27 23:30:27 +0530

    get by ID

--- routers/foodOrder.js
+++ routers/foodOrder.js
@@ -1,14 +1,25 @@
 const router = require('express').Router();
 const Order = require('../models/Orders');
 
-router.get('/order/:orderId', async (req, res) => {
+router.get('/order/:id', async (req, res) => {
     try {
-      const order = await Order.findById(req.params.orderId);
+      const order = await Order.find({id:req.params.id});
       res.status(200).json(order);
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
 });
+
+router.get('/order', async (req, res) => {
+    try {
+      const orders = await Order.find();
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+}
+);
+
 router.post('/order', async (req, res) => {
     const order = new Order(req.body);
     console.log(order , "satra")
